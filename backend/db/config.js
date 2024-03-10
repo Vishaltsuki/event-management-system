caed3b29c3d6023f6680608e945cf9108d4c53f7
vishal <kvishalsrivatsav2004@gmsil.com>
2024-03-10 09:05:46 +0530

    first commit

---
+++ backend/db/config.js
@@ -0,0 +1,2 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://admin:admin@cluster0.sjl4roc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/event-management-system");
